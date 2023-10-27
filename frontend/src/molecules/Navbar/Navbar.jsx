a8eab94da1a65545db601ecb27c6975ee305862b
msaju <mr457277@dal.ca>
2023-10-27 18:19:15 -0300

    change:frontend code for signup and login

--- frontend/src/molecules/Navbar/Navbar.jsx
+++ frontend/src/molecules/Navbar/Navbar.jsx
@@ -3,14 +3,24 @@ import { Link, useLocation } from 'react-router-dom';
 
 export default function Navbar() {
   const { pathname } = useLocation();
+
+  if (pathname === '/' || pathname === '/signup' || pathname === '/signin') {
+    return (
+      <nav className="nav">
+        <Link to="/" className="title">
+          SHELTER
+        </Link>
+      </nav>
+    );
+  }
   return (
     <nav className="nav">
-      <Link to="/" className="title">
-        <span className="nav-title">SHELTER</span>
+      <Link to="/home" className="title">
+        SHELTER
       </Link>
       <ul>
-        <li className={pathname === '/' ? 'active' : ''}>
-          <Link to="/">Home</Link>
+        <li className={pathname === '/home' ? 'active' : ''}>
+          <Link to="/home">Home</Link>
         </li>
 
         <li className={pathname === '/notices' ? 'active' : ''}>
---
+++ frontend/src/organisms/noticeboard/Signin/Signin.jsx
@@ -0,0 +1,141 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useState } from 'react';
+import {useNavigate }from "react-router-dom";
+import axios from "axios";
+
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <h1>
+        Shelter
+      </h1>
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+
+
+const defaultTheme = createTheme();
+
+export default function Signin() {
+  let navigate = useNavigate();
+
+    const [inputs, setInputs] = useState({
+        password: '',
+        email: '',
+      });
+
+      const handleInputChange = (e) => {
+        const { name, value } = e.target;
+          setInputs({
+            ...inputs,
+            [name]: value,
+            showFields: true,
+          });
+       
+      };
+    
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const { password,email} = inputs
+      const data = { email:email, password: password};
+        console.log(data);
+        axios.post("https://shelter-project2.onrender.com/auth/login", data).then((response) => {
+      if (response.data.error) {
+        alert(response.data.error);
+      } else {
+        sessionStorage.setItem("accessToken", response.data.token);
+        navigate("/home");
+      }
+    
+  });
+};
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              onChange={handleInputChange}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              onChange={handleInputChange}
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="/signup" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
+    </ThemeProvider>
+  );
+}
