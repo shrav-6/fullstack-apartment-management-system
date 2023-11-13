5b6f9a0edd81df684bec34dc2b385b334f848bf0
Merin Mary Saju <mr457277@dal.ca>
2023-11-13 17:55:40 -0400

    changed everything for worrking properly

--- frontend/src/organisms/noticeboard/Managers/Notices.service.jsx
+++ frontend/src/organisms/noticeboard/Managers/Notices.service.jsx
@@ -1,102 +1,61 @@
-import React, { useEffect } from 'react';
-import { connect } from 'react-redux';
-import { message } from 'antd';
-import _get from 'lodash/get';
-import Card from '../../../molecules/Card/Card';
-import { useState } from 'react';
-import FilterAltRoundedIcon from '@mui/icons-material/FilterAltRounded';
-import SortRoundedIcon from '@mui/icons-material/SortRounded';
+import axios from 'axios';
+import { http } from '../../../services/http';
+import { noticesService } from '../../../services/urls';
 
-import {
-  // createNotice,
-  // getNoticeById,
-  getAllNotices,
-} from './Notices.service';
-//import { setNotices } from '../data/notice.actions';
 
-function NoticesM() {
+// export async function createNotice(data) {
+//   try {
+//     const response = await axios.post(`${baseUrl}/`, data);
+//     return response.data;
+//   } catch (error) {
+//     console.log(error);
+//     throw error;
+//   }
+// }
 
-  const [data, setData] = useState([]);
+// export async function getNoticeById(id) {
+//   try {
+//     const response = await axios.get(`${baseUrl}/${id}`);
+//     return response.data;
+//   } catch (error) {
+//     console.log(error);
+//     throw error;
+//   }
+// }
 
-  useEffect(() => {
-    getAllNotices()
-    .then(response => setData(response))
-  }, []);
-
-  // const handleCreateNotice = () => {
-  //   const newNotice = {
-  //     title: 'New Notice',
-  //     description: 'This is a new notice',
-  //     authorName: 'John Doe',
-  //     dateAndTime: '2023-10-09T21:51:36.000Z',
-  //   };
-  //   createNotice(newNotice).then((notice) => {
-  //     // setNotices([...notices, notice]);
-  //   });
-  // };
-
-  // const handleEditNotice = (id) => {
-  //   const updatedNotice = {
-  //     title: 'Updated Title',
-  //     description: 'This is an updated notice',
-  //     authorName: 'Jane Doe',
-  //     dateAndTime: '2023-10-10T21:51:36.000Z',
-  //   };
-  //   editNotice(id, updatedNotice).then((notice) => {
-  //     setNotices(notices.map(notice => (notice.id === id ? updatedNotice : notice)));
-  //   });
-  // };
-
-  // const handleDeleteNotice = (id) => {
-  //   deleteNotice(id).then(() => {
-  //     setNotices(notices.filter(notice => notice.id !== id));
-  //   });
-  // };
-
-  const [isDropdownVisible, setDropdownVisible] = useState(false);
-
-  const handleMouseEnter = () => {
-    setDropdownVisible(true);
-  };
-
-  const handleMouseLeave = () => {
-    setDropdownVisible(false);
-  };
-  
-  return (
-    <div>
-      <h1 className="nav-notice">Notices</h1>
-      <div
-        style={{float:"right"}}
-        onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave}>
-        <div>
-          <FilterAltRoundedIcon />
-          {isDropdownVisible && <DropdownFilterMenu notices />}
-        </div>
-        <div>
-          <SortRoundedIcon />
-          {isDropdownVisible && <DropdownSortMenu  notices />}
-        </div>
-      </div>
-      <div className="card">
-        {data.map(notice => (
-           <Card
-           key={notice.id} // Add a unique key
-           notice={notice}
-          />
-        ))}
-      </div>
-    </div>
-  );
+export async function getAllNotices() {
+  try {
+    const response = await axios.get('http://localhost:3001/Notices',
+      {
+        headers: {
+          accessToken: sessionStorage.getItem("accessToken"),
+        },
+      }
+    );
+    console.log(response.data);
+    return response.data.data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 }
 
-// const mapStateToProps = ({ noticeReducer }) => ({
-//   allNotices: _get(noticeReducer, 'allNotices'),
-// });
+// export async function editNotice(id, data) {
+//   try {
+//     const response = await axios.put(`${baseUrl}/${id}`, data);
+//     return response.data;
+//   } catch (error) {
+//     console.log(error);
+//     throw error;
+//   }
+// }
 
-// const mapDispatchToProps = dispatch => ({
-//   onSetNotices: payload => dispatch(setNotices(payload)),
-// });
-
-export default NoticesM;
+// export async function deleteNotice(id) {
+//   try {
+//     const response = await axios.delete(`${baseUrl}/${id}`);
+//     return response.data;
+//   } catch (error) {
+//     console.log(error);
+//     throw error;
+//   }
+// }
