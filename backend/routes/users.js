fb1295178dd59516067477e18d9c4c515f861d46
Merin Mary Saju <mr457277@dal.ca>
2023-11-13 16:45:24 -0400

    For adding new user Guest in backend

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,68 +1,76 @@
 const express = require("express");
 const router = express.Router();
-const { users,tenants,managers,buildings,guests } = require("../models");
+const { users,tenants,managers,buildings,guests} = require("../models");
 const bcrypt = require("bcrypt");
 const { sign } = require("jsonwebtoken");
 
 router.post("/signup", async (req, res) => {
   const { userName,email,name, password,role,phoneNumber,address} = req.body;
   const user1=await users.findOne({ where: { email: email } });
-  if(user1==null) {
-    if(role=="Tenant") {
-      const buildingName=req.body.buildingName;
-      const building = await buildings.findOne({ where: { buildingName:buildingName } });
-      const manager = await managers.findOne({ where: { id:building.managerId } });
-      if(manager!=null && building!=null) {
-        const hash=await bcrypt.hash(password, 10)
+  if(user1==null){
+  if(role=="Tenant"){
+    const buildingName=req.body.buildingName;
+    const building = await buildings.findOne({ where: { buildingName:buildingName } });
+    const manager = await managers.findOne({ where: { id:building.managerId } });
+    if(manager!=null && building!=null){
+      const hash=await bcrypt.hash(password, 10)
+      await users.create({
+        userName: userName,
+        password: hash,
+        email:email,
+        role:role,
+      
+      });
+    
+     const user=await users.findOne({ where: { email: email } });
+     const tenant={"name":name,"phoneNumber":phoneNumber,"address":address,managerId:manager.id,"buildingId":building.id,"apartmentNumber":req.body.apartmentNumber,"userId":user.id};
+     await tenants.create(tenant);
+     res.json({"success": true,
+      "message": "user profile created successfully"});
+    }
+    else{
+      res.status(500).json({"success": false,
+      "message": "Not successfull,invalid buildingname"});
+    }
+  }
+    else if (role=="Manager"){
+      const hash = await bcrypt.hash(password, 10);
         await users.create({
           userName: userName,
           password: hash,
           email:email,
-          role:role,      
-        });    
-      const user=await users.findOne({ where: { email: email } });
-      const tenant={"name":name,"phoneNumber":phoneNumber,"address":address,managerId:manager.id,"buildingId":building.id,"apartmentNumber":req.body.apartmentNumber,"userId":user.id};
-      await tenants.create(tenant);
-      res.json({"success": true, "message": "user profile created successfully"});
-      }
-      else {
-        res.json({"success": false, "error": "Not successfull,invalid buildingname"});
-      }
-    }
-    else if (role=="Manager"){
-      const hash = await bcrypt.hash(password, 10);
-      await users.create({
-        userName: userName,
-        password: hash,
-        email:email,
-        role:role,
-      });
+          role:role,
+        });
       const user=await users.findOne({ where: { email: email } });
       const manager={"name":name,"phoneNumber":phoneNumber,"address":address,"userId":user.id};
-      await managers.create(manager);
-       res.json({"success": true,"message": "user profile created successfully"});
-    }    
-    else if(role == "Guest") {
-      const hash = await bcrypt.hash(password, 10);
-      await users.create({
-        userName: userName,
-        password: hash,
-        email:email,
-        role:role,
-      });
-      const user=await users.findOne({ where: { email: email } });
-      const guest={"name":name,"phoneNumber":phoneNumber,"userId":user.id};
-        await guests.create(guest);
+        await managers.create(manager);
         res.json({"success": true,
         "message": "user profile created successfully"});
-    }
-    else{
-      res.json({"success": false, "error": "Not successful,no specific roles"});
-    } 
+      }
+      else if (role=="Guest"){
+        const hash = await bcrypt.hash(password, 10);
+          await users.create({
+            userName: userName,
+            password: hash,
+            email:email,
+            role:role,
+          });
+        const user=await users.findOne({ where: { email: email } });
+        const guest={"name":name,"phoneNumber":phoneNumber,"userId":user.id};
+          await guest.create(guest);
+          res.json({"success": true,
+          "message": "user profile created successfully"});
+        }
+
+  else{
+    res.status(500).json({"success": false,
+        "message": "Not successful,no specific roles"});
   }
-  else {
-    res.json({"success": false, "error": "Email already used for user!"});
-  }     
+}
+else{
+  res.status(500).json({"success": false,
+        "message": "Email is already registered"});
+}
 });
 
 router.post('/login', async (req, res) => {
