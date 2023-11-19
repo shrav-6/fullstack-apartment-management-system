25d0c2b59ad46da4bfb71e409ff9318052f279c2
DISHA ANAND <ds442718@dal.ca>
2023-11-18 20:15:32 -0400

    Update fetchData.jsx

--- frontend/src/organisms/PublicView/fetchData.jsx
+++ frontend/src/organisms/PublicView/fetchData.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const fetchData = async () => {
   try {
-    const response = await axios.get("http://localhost:3001/Listings/all");
+    const response = await axios.get("http://172.17.0.237:8074/Listings/all");
     console.log(response.data);
     if (response.status === 200) {
       return response.data; // Return the data if the response status is 200 (OK).
